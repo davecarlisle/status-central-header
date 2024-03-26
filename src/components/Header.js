6a2ad92afced3be6084bf3ada21344d3d6d13f1e
Dave Carlisle <dave.carlisle@hpe.com>
2024-03-25 22:06:22 -0700

    Main link patch...

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ const AppHeader = () => {
   return (
     <Header fill="horizontal" pad={{ horizontal: 'medium' }} background="background-front">
       <Tip content="Status Central home...">
-        <Button onClick={() => { goNav("https://hpetest01.statuspage.io/"); }}>
+        <Button onClick={() => { goNav("/"); }}>
           <Box direction="row" align="start" gap="medium" pad={{ vertical: 'small' }} responsive={false}>
             <Box height="24px" justify="center">
               <HpeIcon color="brand"/>
